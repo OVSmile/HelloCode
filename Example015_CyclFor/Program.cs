f6fa166fb81e887019c9f5097ee17aee1c247b8d
Oleg Smile <valsov-9988@mail.ru>
2022-10-09 23:43:13 +0300

    Cycle for, sort, replace

--- Example015_CyclFor/Program.cs
+++ Example015_CyclFor/Program.cs
@@ -53,3 +53,83 @@ newText = Replace(newText, 'к', 'К');
 newText = Replace(newText, 'С', 'с');
 
 Console.WriteLine(newText);
+
+// ===== КОНЕЦ работы с текстом
+
+// Сортировка чисел от меньшего к большему
+
+int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length -1; i++)
+    {
+        int minPosition = i;
+        
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition]) minPosition = j;     
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;      
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
+
+
+// Сортировка чисел от большего к меньшему
+
+int[] arr1 = {14, 5, 6, 2, 8, 4, 7, 56, 7};
+
+void PrintArray1(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort1(int[] array)
+{
+    for (int i = array.Length - 1; i > -1; i--)
+    {
+        int minPosition = i;
+        
+        for (int j = i; j > -1; j--)
+        {
+            if(array[j] < array[minPosition]) minPosition = j;     
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;      
+    }
+}
+
+PrintArray1(arr1);
+SelectionSort1(arr1);
+
+PrintArray1(arr1);
+
+
+
+
+
